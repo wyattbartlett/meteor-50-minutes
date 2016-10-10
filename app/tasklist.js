0a3d8f773da4d4dd332ed8445fe4961725df9118
Wyatt Bartlett <wyattmmbartlett@gmail.com>
2016-10-09 22:08:21 -1000

    Task 4: Tasklist authentication

--- app/tasklist.js
+++ app/tasklist.js
@@ -12,10 +12,7 @@ if (Meteor.isClient) {
     'submit .add-task': function (event) {
       let name = event.target.name.value;
 
-      Tasks.insert({
-        name: name,
-        createdAt: new Date()
-      });
+      Meteor.call('addTask', name);
 
       event.target.name.value = '';
       return false;
@@ -23,7 +20,7 @@ if (Meteor.isClient) {
 
     "click .delete-task": function (event) {
       if (confirm('Delete Task?')) {
-        Tasks.remove(this._id);
+        Meteor.call('deleteTask', this._id);
       }
       return false;
     }
@@ -34,3 +31,23 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
 
 }
+
+Meteor.methods({
+
+  addTask: function(name){
+    if(!Meteor.userId()){
+      throw new Meteor.Error('No Access');
+    }
+
+    Tasks.insert({
+      name: name,
+      createdAt: new Date(),
+      userID: Meteor.userId(),
+    });
+  },
+
+  deleteTask: function(taskId){
+    Tasks.remove(taskId);
+  }
+
+})
