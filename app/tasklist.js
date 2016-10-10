7d591c0946c4ef018b5e611d97f6159adc84b5fa
Wyatt Bartlett <wyattmmbartlett@gmail.com>
2016-10-09 20:50:05 -1000

    Task 2: Tasklist setup

--- app/tasklist.js
+++ app/tasklist.js
@@ -1,7 +1,11 @@
-Tasks
+Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient){
-
+  Template.tasks.helpers({
+    tasks: function(){
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
 }
 
 if (Meteor.isServer){
