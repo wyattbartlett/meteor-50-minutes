d7b67d409b72f6cd884096702bb4c7750ffc042c
Wyatt Bartlett <wyattmmbartlett@gmail.com>
2016-10-09 21:12:12 -1000

    Task 3: Tasklist form

--- app/tasklist.js
+++ app/tasklist.js
@@ -1,13 +1,36 @@
 Tasks = new Mongo.Collection('tasks');
 
-if (Meteor.isClient){
+if (Meteor.isClient) {
   Template.tasks.helpers({
-    tasks: function(){
-      return Tasks.find({}, {sort: {createdAt: -1}});
+    tasks: function () {
+      return Tasks.find({}, { sort: { createdAt: -1 } });
     }
+  });
+
+  Template.tasks.events({
+
+    'submit .add-task': function (event) {
+      let name = event.target.name.value;
+
+      Tasks.insert({
+        name: name,
+        createdAt: new Date()
+      });
+
+      event.target.name.value = '';
+      return false;
+    },
+
+    "click .delete-task": function (event) {
+      if (confirm('Delete Task?')) {
+        Tasks.remove(this._id);
+      }
+      return false;
+    }
+
   });
 }
 
-if (Meteor.isServer){
+if (Meteor.isServer) {
 
 }
